43f9d29e9fada73a4aefcfd3ec20fe63365fcd97
Alejo Amiras <alejo.amiras@gmail.com>
2021-02-10 10:54:55 -0300

    feat: improves linting with solhint and prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,27 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ganache';
 import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-solhint';
 import { removeConsoleLog } from 'hardhat-preprocessor';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-
-const config = require('./.config.json');
-
-let hardhat = {};
-
-if (process.env.FORK) { 
-  hardhat = {
-    forking: { 
-      url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`
-    }
-  };
-}
+import { utils } from 'ethers';
+import config from './.config.json';
 
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
-    hardhat,
+    hardhat: {
+      enabled: process.env.FORK ? true : false,
+      accounts: [
+        {
+          privateKey: config.accounts.mainnet.privateKey,
+          balance: utils.parseEther('1000').toString(),
+        },
+      ],
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
+      },
+    },
     localMainnet: {
       url: 'http://127.0.0.1:8545',
       accounts: [config.accounts.mainnet.privateKey],
@@ -31,34 +31,37 @@ module.exports = {
       url: `https://eth-ropsten.alchemyapi.io/v2/${config.alchemy.ropsten.apiKey}`,
       accounts: [config.accounts.ropsten.privateKey],
       gasMultiplier: 1.1,
-      gasPrice: 'auto'
+      gasPrice: 'auto',
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
       accounts: [config.accounts.mainnet.privateKey],
       gasMultiplier: 1.1,
-      gasPrice: 'auto'
-    }
+      gasPrice: 'auto',
+    },
   },
   solidity: {
     version: '0.6.8',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 200,
       },
-    }
+    },
   },
   mocha: {
-    timeout: 100000
+    timeout: 100000,
   },
   gasReporter: {
-    enabled: (process.env.REPORT_GAS) ? true : false
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: 'USD',
+    gasPrice: 200,
+    coinmarketcap: `${config.coinmarketcap.apiKey}`,
   },
   preprocess: {
     eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
   },
   etherscan: {
-    apiKey: `${config.etherscan.apiKey}`
-  }
+    apiKey: `${config.etherscan.apiKey}`,
+  },
 };
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe('Greeter', function() {
-  it('Should return the new greeting once it\'s changed', async function() {
+describe('Greeter', function () {
+  it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory('Greeter');
     const greeter = await Greeter.deploy('Hello, world!');
     expect(await greeter.greet()).to.equal('Hello, world!');
