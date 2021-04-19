ce4ee3bdb8bdbe7ea9335869cfaa351193c2adb3
Alejo Amiras <alejo.amiras@gmail.com>
2021-04-19 11:39:34 -0300

    ci: adds github action to test and lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,67 +1,54 @@
+import 'dotenv/config';
+import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-ganache';
 import '@nomiclabs/hardhat-etherscan';
 import { removeConsoleLog } from 'hardhat-preprocessor';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-import { utils } from 'ethers';
-import config from './.config.json';
 
 module.exports = {
   defaultNetwork: 'hardhat',
-  networks: {
-    hardhat: {
-      enabled: process.env.FORK ? true : false,
-      accounts: [
-        {
-          privateKey: config.accounts.mainnet.privateKey,
-          balance: utils.parseEther('1000').toString(),
+  networks: process.env.TEST
+    ? {}
+    : {
+        hardhat: {
+          forking: {
+            enabled: process.env.FORK ? true : false,
+            url: process.env.MAINNET_HTTPS_URL,
+          },
         },
-      ],
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
+        localMainnet: {
+          url: process.env.LOCAL_MAINNET_HTTPS_URL,
+          accounts: [process.env.LOCAL_MAINNET_PRIVATE_KEY],
+        },
+        mainnet: {
+          url: process.env.MAINNET_HTTPS_URL,
+          accounts: [process.env.MAINNET_PRIVATE_KEY],
+          gasPrice: 'auto',
+        },
       },
-    },
-    localMainnet: {
-      url: 'http://127.0.0.1:8545',
-      accounts: [config.accounts.mainnet.privateKey],
-      gasMultiplier: 1.1,
-    },
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${config.alchemy.ropsten.apiKey}`,
-      accounts: [config.accounts.ropsten.privateKey],
-      gasMultiplier: 1.1,
-      gasPrice: 'auto',
-    },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${config.alchemy.mainnet.apiKey}`,
-      accounts: [config.accounts.mainnet.privateKey],
-      gasMultiplier: 1.1,
-      gasPrice: 'auto',
-    },
-  },
   solidity: {
-    version: '0.6.8',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
-  },
-  mocha: {
-    timeout: 100000,
+    ],
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
-    currency: 'USD',
-    gasPrice: 200,
-    coinmarketcap: `${config.coinmarketcap.apiKey}`,
+    currency: process.env.COINMARKETCAP_DEFAULT_CURRENCY,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   preprocess: {
-    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
+    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat'),
   },
   etherscan: {
-    apiKey: `${config.etherscan.apiKey}`,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
