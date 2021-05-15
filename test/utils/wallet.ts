b1a75bd8b73edd4378dfa2931d6aeb65c49b3440
Alejo Amiras <alejo.amiras@gmail.com>
2021-05-15 19:10:01 -0300

    feat: adds wallet impersonate

--- test/utils/wallet.ts
+++ test/utils/wallet.ts
@@ -1,5 +1,14 @@
 import { Wallet } from 'ethers';
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
+import { JsonRpcSigner } from '@ethersproject/providers';
+
+const impersonate = async (address: string): Promise<JsonRpcSigner> => {
+  await network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [address],
+  });
+  return ethers.provider.getSigner(address);
+};
 
 const generateRandom = async () => {
   const wallet = (await Wallet.createRandom()).connect(ethers.provider);
@@ -12,6 +21,7 @@ const generateRandomAddress = async () => {
 };
 
 export default {
+  impersonate,
   generateRandom,
   generateRandomAddress,
 };
