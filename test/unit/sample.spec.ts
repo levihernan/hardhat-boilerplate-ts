cdf345a1b0835bd0accf24ff19bfae784c7515c7
Hernan Levi <levi.hernan@gmail.com>
2021-05-26 13:18:20 +0200

    swapper 5.1 excersise

--- test/unit/sample.spec.ts
+++ test/unit/sample.spec.ts
@@ -1,14 +1,18 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe('Greeter', function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory('Greeter');
-    const greeter = await Greeter.deploy('Hello, world!');
-    expect(await greeter.greet()).to.equal('Hello, world!');
+let tokenA = '0xdac17f958d2ee523a2206206994597c13d831ec7'; //USDT-mainnet
+let tokenB = '0x8b9c35c79af5319c70dd9a3e3850f368822ed64e'; //DOGE-mainnet
 
-    await greeter.setGreeting('Hola, mundo!');
-    expect(await greeter.greet()).to.equal('Hola, mundo!');
+describe('Greeter', function () {
+  it('Should return token A & B once its deployed', async function () {
+    const Swapper = await ethers.getContractFactory('Swapper');
+    const swapper = await Swapper.deploy(tokenA, tokenB);
+
+    // expect(await swapper.fromToken()).to.equal( tokenA );
+    // expect(await swapper.toToken()).to.equal( tokenB );
+    expect((await swapper.fromToken()).toLowerCase()).to.equal(tokenA.toLowerCase());
+    expect((await swapper.toToken()).toLowerCase()).to.equal(tokenB.toLowerCase());
 
     await new Promise((res) => setTimeout(res, 5000));
   });
